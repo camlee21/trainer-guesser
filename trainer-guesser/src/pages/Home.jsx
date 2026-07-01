7dfdc75cefcd273efe4654c1425bd09250d2f699
Cameron Lee <20555470@student.curtin.edu.au>
2026-07-02 05:13:05 +0800

    Changed streak location

--- trainer-guesser/src/pages/Home.jsx
+++ trainer-guesser/src/pages/Home.jsx
@@ -96,12 +96,14 @@ function DailyMode() {
 
   return (
     <>
-      {streak >= 2 && (
-        <div className="streak-banner">🔥 {streak} day streak</div>
-      )}
       <main className="main-layout">
         <div className="trainer-panel">
           <div className="trainer-frame-wrapper">
+            {streak >= 2 && (
+              <div className="streak-banner" style={{ marginBottom: '8px' }}>
+                🔥 {streak} day streak!
+              </div>
+            )}
             <DayBadge
               dayNumber={trainer.dayNumber}
               isProvided={trainer.isProvided}
